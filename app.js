bfad6b124c57e7ea28a667ae29558261ee2de05d
Pavel Lisitsyn <pavellisitsyn69@gmail.com>
2022-07-23 19:19:11 +0300

    configuring node env

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const config = require("config");
 
 const PORT = config.get("port") ?? 8080;
 
+// if (process.env.NODE_ENV === "production") {
+//     console.log("production");
+// } else {
+//     console.log("development");
+// }
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
     console.log(chalk.green(`Server has been started on ${PORT} port`));
