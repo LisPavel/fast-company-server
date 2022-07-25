e89e159a3e356ef1753b568f5230d04e419f4ad0
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-07-25 18:23:28 +0300

    base routes (10)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 const initDatabase = require("./startUp/initDatabase");
+const routes = require("./routes");
 
 const PORT = config.get("port") ?? 8080;
 
@@ -17,6 +18,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", routes);
 
 async function start(params) {
     try {
