28982f690c61722444457d5aa97dd54d097643f8
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-07-25 17:57:22 +0300

    user schema

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const schema = new Schema(
+    {
+        name: {
+            type: String,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: { type: String, required: true },
+        completedMeetings: Number,
+        image: String,
+        rate: Number,
+        sex: {
+            type: String,
+            enum: ["male", "female", "other"],
+        },
+        profession: {
+            type: Schema.Types.ObjectId,
+            ref: "Profession",
+        },
+        qualities: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Quality",
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+module.exports = model("Quality", schema);
