ff98c1d953b626968a11293b76b0c33515f9943a
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-12-19 18:01:32 +0300

    redo static app

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const chalk = require("chalk");
 const mongoose = require("mongoose");
 const config = require("config");
 const cors = require("cors");
+const path = require("path");
 
 const initDatabase = require("./startUp/initDatabase");
 const routes = require("./routes");
 
 const PORT = config.get("port") ?? 8080;
-
-// if (process.env.NODE_ENV === "production") {
-//     console.log("production");
-// } else {
-//     console.log("development");
-// }
-
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static(path.join(__dirname, "client")));
+    const indexPath = path.join(__dirname, "client", "index.html");
+    app.get("*", (req, res) => res.sendFile(indexPath));
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
