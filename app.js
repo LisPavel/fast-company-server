c53a75b6442d2dbba9f025966764d8b20362204c
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-12-20 12:46:16 +0300

    fixed bug

--- app.js
+++ app.js
@@ -11,17 +11,16 @@ const routes = require("./routes");
 const PORT = config.get("port") ?? 8080;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use("/api", routes);
+
 if (process.env.NODE_ENV === "production") {
     app.use("/", express.static(path.join(__dirname, "client")));
     const indexPath = path.join(__dirname, "client", "index.html");
     app.get("*", (req, res) => res.sendFile(indexPath));
 }
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-
-app.use("/api", routes);
 
 async function start(params) {
     try {
